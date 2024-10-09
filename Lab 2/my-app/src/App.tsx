d4d067f67db1494d1c951dadaac46a62b1decbf0
Milo Nguyen <thunguyenminh177@gmail.com>
2024-10-09 04:18:19 -0700

    excercise 4.1.2 done

--- Lab 2/my-app/src/App.tsx
+++ Lab 2/my-app/src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app-container'>
+          <form className="note-form">
+              <div><input placeholder="Note Title"></input></div>
+
+              <div><textarea></textarea></div>
+
+              <div>
+                  <button type="submit">Create Note</button>
+              </div>
+          </form>
+          <div className="note-individual">
+              <button>
+                  x
+              </button>
+              <h2> 1st Note Title </h2>
+              <p> 1st Note Content </p>
+              <p> 1st Note Label </p>
+          </div>
+      </div>
+
   );
 }
 
