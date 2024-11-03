e5a7b1af7630d0048980f56125c681a11df88c18
Milo Nguyen <mtn022@ucsd.edu>
2024-11-02 19:54:27 -0700

    test should fail

--- Lab 4/src/App.test.tsx
+++ Lab 4/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i); // there is no 'learn react' element
+  const linkElement = screen.getByText(/dummy element/i); // this doesn't exist. test should fail.
   expect(linkElement).toBeInTheDocument();
 });
